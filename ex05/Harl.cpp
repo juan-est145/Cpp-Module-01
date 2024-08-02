36a53247cf3479dc5861f4a1be4fd7c76c9cc95a
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-08-02 16:23:08 +0200

    Harl private methods have been defined

--- ex05/Harl.cpp
+++ ex05/Harl.cpp
@@ -6,8 +6,35 @@
 /*   By: juestrel <juestrel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/02 16:10:53 by juestrel          #+#    #+#             */
-/*   Updated: 2024/08/02 16:12:01 by juestrel         ###   ########.fr       */
+/*   Updated: 2024/08/02 16:22:47 by juestrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
+
+void Harl::_debug(void)
+{
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;
+}
+void Harl::_info(void)
+{
+	std::cout << "I cannot believe adding extra bacon costs more money. You did not put enough bacon in my burger! If you did, I would not be asking for more!" << std::endl;
+}
+
+void Harl::_warning(void)
+{
+	std::cout << "I think I deserve to have some extra bacon for free. I have been coming for years whereas you started working here since last month." << std::endl;
+}
+
+void Harl::_error(void)
+{
+	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+}
+
+Harl::Harl(void) {}
+
+Harl::~Harl(void) {}
+
+void Harl::complain(std::string level)
+{
+}
