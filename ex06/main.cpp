a9f3975554ad034124a534dd983293b43e073c48
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-08-04 14:10:11 +0200

    Harl now complains again

--- ex06/main.cpp
+++ ex06/main.cpp
@@ -6,7 +6,7 @@
 /*   By: juestrel <juestrel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/02 16:10:50 by juestrel          #+#    #+#             */
-/*   Updated: 2024/08/04 13:53:21 by juestrel         ###   ########.fr       */
+/*   Updated: 2024/08/04 14:09:42 by juestrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,8 @@ int main(int argc, char *argv[])
 {
 	Harl harl;
 	unsigned int index = 0;
-	
+	std::string levels[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+
 	if (argc != 2)
 	{
 		std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
@@ -25,14 +26,26 @@ int main(int argc, char *argv[])
 	std::string argument = argv[1];
 	for (unsigned int i = 0; i < argument.length(); i++)
 		toupper(argument[i]);
-	switch (expression)
+	index = (unsigned int)levels->find(argument);
+	for (; index < levels->length(); index++)
 	{
-	case /* constant-expression */:
-		/* code */
-		break;
-	
-	default:
-		break;
+		switch (index)
+		{
+		case 0:
+			harl.complain("DEBUG");
+			break;
+		case 1:
+			harl.complain("INFO");
+			break;
+		case 2:
+			harl.complain("WARNING");
+			break;
+		case 3:
+			harl.complain("ERROR");
+			break;
+		default:
+			break;
+		}
 	}
 	return (0);
 }
