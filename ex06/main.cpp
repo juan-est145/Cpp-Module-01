c562a68c8d99142f2db7b3e41a27764f173df4cc
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-08-04 15:00:54 +0200

    ex06 is done

--- ex06/main.cpp
+++ ex06/main.cpp
@@ -6,48 +6,75 @@
 /*   By: juestrel <juestrel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/02 16:10:50 by juestrel          #+#    #+#             */
-/*   Updated: 2024/08/04 14:11:35 by juestrel         ###   ########.fr       */
+/*   Updated: 2024/08/04 14:59:54 by juestrel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
 
+std::string getString(char *argv)
+{
+	std::string argument = argv;
+	for (unsigned int i = 0; i < argument.length(); i++)
+		argument[i] = toupper(argument[i]);
+	return (argument);
+}
+
+size_t findIndex(const char *string, std::string (&array)[4])
+{
+	for (size_t i = 0; i < 4; i++)
+	{
+		if (string == array[i])
+			return (i);
+	}
+	return (std::string::npos);
+}
+
 int main(int argc, char *argv[])
 {
-	Harl harl;
-	unsigned int index = 0;
-	std::string levels[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
-
 	if (argc != 2)
 	{
 		std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
-		return (1);
+		exit(1);
 	}
-	std::string argument = argv[1];
-	for (unsigned int i = 0; i < argument.length(); i++)
-		toupper(argument[i]);
-	index = (unsigned int)levels->find(argument);
+
+	Harl harl;
+	size_t index;
+	std::string levels[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	std::string argument = getString(argv[1]);
+
+	if (index = findIndex(argument.c_str(), levels), index == std::string::npos)
+	{
+		std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+		exit(1);
+	}
 	for (; index < levels->length(); index++)
 	{
 		switch (index)
 		{
 		case 0:
+			std::cout << "[ DEBUG ]\n";
 			harl.complain("DEBUG");
+			std::cout << std::endl;
 			break;
 		case 1:
+			std::cout << "[ INFO ]\n";
 			harl.complain("INFO");
+			std::cout << std::endl;
 			break;
 		case 2:
+			std::cout << "[ WARNING ]\n";
 			harl.complain("WARNING");
+			std::cout << std::endl;
 			break;
 		case 3:
+			std::cout << "[ ERROR ]\n";
 			harl.complain("ERROR");
+			std::cout << std::endl;
 			break;
 		default:
-			return (0);
 			break;
 		}
 	}
-		std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
 	return (0);
 }
